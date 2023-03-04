e4401b653a057ef5727df85e2a7173cd0ed0e688
joeggg <ninjoetsu@gmail.com>
2023-03-04 18:40:29 +0000

    more efficient reddit command using async

--- bot_worker/reddit.py
+++ bot_worker/reddit.py
@@ -1,6 +1,8 @@
+import asyncio
 import random
 import time
-import requests
+
+import aiohttp
 
 from .config import REDDIT_CONF
 
@@ -9,23 +11,29 @@ async def meme_of_day() -> tuple[int, dict]:
     """
     returns image url with title, subreddit and author -randomly selected from output of scrape_subreddit
     """
-    ##get authorisation
-    set_auth()
+    async with aiohttp.ClientSession() as session:
+        ##get authorisation
+        await set_auth(session)
 
-    memes = []
-    for sub in REDDIT_CONF.subreddits:
-        memes.extend(scrape_subreddit(sub, time="week", lim=10))
+        coros = [
+            scrape_subreddit(session, sub, time="week", lim=10) for sub in REDDIT_CONF.subreddits
+        ]
+
+        memes = []
+        for result in await asyncio.gather(*coros):
+            memes.extend(result)
 
     selection = random.randrange(0, len(memes))
     return 0, memes[selection]
 
 
-def set_auth() -> None:
+async def set_auth(session: aiohttp.ClientSession) -> None:
     # Only auth if token is expired
     if time.time() < REDDIT_CONF.auth_time + (100 * 60):
         return
+
     # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-    auth = requests.auth.HTTPBasicAuth(REDDIT_CONF.creds["client_id"], REDDIT_CONF.creds["secret"])
+    auth = aiohttp.BasicAuth(REDDIT_CONF.creds["client_id"], REDDIT_CONF.creds["secret"])
 
     # here we pass our login method (password), username, and password
     data = {
@@ -38,13 +46,11 @@ def set_auth() -> None:
     headers = {"User-Agent": "botblue"}
 
     # send our request for an OAuth token
-    res = requests.post(
+    async with session.post(
         "https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers
-    )
-
-    # print(res)
-    # convert response to JSON and pull access_token value
-    token = res.json()["access_token"]
+    ) as res:
+        # convert response to JSON and pull access_token value
+        token = (await res.json())["access_token"]
 
     # add authorization to our headers dictionary
     REDDIT_CONF.auth_headers = {**headers, **{"Authorization": f"bearer {token}"}}
@@ -53,8 +59,12 @@ def set_auth() -> None:
     # while the token is valid (~2 hours) we just add headers=headers to our requests
 
 
-def scrape_subreddit(
-    subreddit: str, time: str = "week", lim: int = 10, controversial: bool = False
+async def scrape_subreddit(
+    session: aiohttp.ClientSession,
+    subreddit: str,
+    time: str = "week",
+    lim: int = 10,
+    controversial: bool = False,
 ) -> list[dict]:
     """
     function that scrapes subreddit posts and collects images
@@ -76,15 +86,16 @@ def scrape_subreddit(
     cat = "controversial" if controversial else "top"
 
     ##make the request
-    res = requests.get(
+    async with session.get(
         f"https://oauth.reddit.com/r/{subreddit}/{cat}",
         headers=REDDIT_CONF.auth_headers,
-        params=param_dict,  # type: ignore
-    )
+        params=param_dict,
+    ) as res:
+        results = await res.json()
     # res objct contains listings data
     data = []
     ##iterate through top page of subreddit
-    for item in res.json()["data"]["children"]:
+    for item in results["data"]["children"]:
         ##check item is a post, not comment/user etc
         if item["kind"] == "t3":
             ##if post, then we can carry on
